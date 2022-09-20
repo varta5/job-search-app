86c16275b1ce14b1d502c3ac414683b52c20da24
varta5 <varga.tamas.pro@gmail.com>
2022-09-20 15:45:02 +0200

    add AppClient model and respective Jpa repository

---
+++ src/main/java/com/vargatamas/jobsearchapp/models/AppClient.java
@@ -0,0 +1,19 @@
+package com.vargatamas.jobsearchapp.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class AppClient {
+
+    @Id
+    private Long id;
+
+    private String apiKey;
+    private String name;
+    private String eMailAddress;
+
+    public AppClient() {
+    }
+
+}
---
+++ src/main/java/com/vargatamas/jobsearchapp/repositories/AppClientRepository.java
@@ -0,0 +1,9 @@
+package com.vargatamas.jobsearchapp.repositories;
+
+import com.vargatamas.jobsearchapp.models.AppClient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppClientRepository extends JpaRepository<AppClient, Long> {
+}
