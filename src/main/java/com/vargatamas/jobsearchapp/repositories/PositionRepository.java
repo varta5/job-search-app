00345ce9c5e03f47bb154d61fc4919ba821760c2
varta5 <varga.tamas.pro@gmail.com>
2022-09-22 15:56:37 +0200

    create repository for job position model

---
+++ src/main/java/com/vargatamas/jobsearchapp/repositories/PositionRepository.java
@@ -0,0 +1,9 @@
+package com.vargatamas.jobsearchapp.repositories;
+
+import com.vargatamas.jobsearchapp.models.Position;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PositionRepository extends JpaRepository<Position, Long> {
+}
