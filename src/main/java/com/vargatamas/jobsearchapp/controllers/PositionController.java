220a8e4ce531974c74a67ac4b5f356e07359db87
varta5 <varga.tamas.pro@gmail.com>
2022-09-23 16:09:02 +0200

    create endpoint method getPosition in PositionController

--- src/main/java/com/vargatamas/jobsearchapp/controllers/PositionController.java
+++ src/main/java/com/vargatamas/jobsearchapp/controllers/PositionController.java
@@ -1,5 +1,6 @@
 package com.vargatamas.jobsearchapp.controllers;
 
+import com.vargatamas.jobsearchapp.dtos.GetPositionResponseDTO;
 import com.vargatamas.jobsearchapp.dtos.PostPositionRequestDTO;
 import com.vargatamas.jobsearchapp.dtos.PostPositionResponseDTO;
 import com.vargatamas.jobsearchapp.models.AppClient;
@@ -7,12 +8,16 @@ import com.vargatamas.jobsearchapp.services.AppClientService;
 import com.vargatamas.jobsearchapp.services.PositionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/position")
 public class PositionController {
 
     private final AppClientService appClientService;
@@ -24,12 +29,20 @@ public class PositionController {
         this.positionService = positionService;
     }
 
-    @PostMapping("/position")
+    @PostMapping("")
     public ResponseEntity<PostPositionResponseDTO> createPosition(
             @RequestHeader(name = "Authorization", required = false) String apiKey,
             @RequestBody(required = false) PostPositionRequestDTO requestDTO) {
         AppClient appClient = appClientService.getAppClientById(apiKey);
         return ResponseEntity.ok(positionService.savePosition(requestDTO, appClient));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GetPositionResponseDTO> getPosition(
+            @RequestHeader(name = "Authorization", required = false) String apiKey,
+            @PathVariable("id") Long positionId) {
+        appClientService.getAppClientById(apiKey);
+        return ResponseEntity.ok(positionService.findPositionById(positionId));
     }
 
 }
