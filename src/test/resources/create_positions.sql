79c0b949bc495c07c91efc647c9379f79d0abdc2
varta5 <varga.tamas.pro@gmail.com>
2022-09-23 20:17:51 +0200

    add integration tests for GET /position/search endpoint

--- src/test/resources/create_positions.sql
+++ src/test/resources/create_positions.sql
@@ -1,2 +1,4 @@
 INSERT INTO positions (id, name, job_location, app_client_api_key) VALUES
-    (2, 'Existing Test Position name', 'Location of Existing Test Position', 'f5736401-bcf0-4f04-b896-01777e5d0bb1');
+    (2, 'Existing Test Position name', 'Location of Existing Test Position', 'f5736401-bcf0-4f04-b896-01777e5d0bb1'),
+    (3, 'Another job position', 'Office in London, UK', 'f5736401-bcf0-4f04-b896-01777e5d0bb1'),
+    (4, 'Junior Java backend developer', 'Home office', 'f5736401-bcf0-4f04-b896-01777e5d0bb1');
