b6d3fc9e880a60d830d829a201a77579d0735c93
varta5 <varga.tamas.pro@gmail.com>
2022-09-23 16:44:20 +0200

    add integration tests for GET /position/{id} endpoint

--- src/test/java/com/vargatamas/jobsearchapp/integrationtests/PositionIntegrationTest.java
+++ src/test/java/com/vargatamas/jobsearchapp/integrationtests/PositionIntegrationTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -20,7 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Sql(value = {"/create_clients.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = {"/create_clients.sql", "/create_positions.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(value = {"/delete_positions.sql", "/delete_clients.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class PositionIntegrationTest {
 
@@ -77,6 +78,49 @@ public class PositionIntegrationTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(mediaType))
                 .andExpect(content().string(containsString("/position/")));
+    }
+
+    // endregion
+
+    // region integration tests for GET /position/{id} endpoint
+
+    @Test
+    public void getPosition_WithoutAuthenticationHeader_ReturnsBadRequest() throws Exception {
+        mockMvc.perform(get("/position/2"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(mediaType))
+                .andExpect(jsonPath("$.error").value("Field 'apiKey' is missing or empty"));
+    }
+
+    @Test
+    public void getPosition_ApiKeyIsNotRegistered_ReturnsUnauthorized() throws Exception {
+        mockMvc.perform(get("/position/2")
+                        .header("Authorization", "e5736401-bcf0-4f04-b896-01777e5d0bb1"))
+                .andExpect(status().isUnauthorized())
+                .andExpect(content().contentType(mediaType))
+                .andExpect(jsonPath("$.error").value(
+                        "'apiKey' is not registered in the system. Please provide already saved identifier"));
+    }
+
+    @Test
+    public void getPosition_PositionIdIsNotExisting_ReturnsBadRequest() throws Exception {
+        mockMvc.perform(get("/position/1")
+                        .header("Authorization", "f5736401-bcf0-4f04-b896-01777e5d0bb1"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(mediaType))
+                .andExpect(jsonPath("$.error").value("No position exists in our database with the ID provided"));
+    }
+
+    @Test
+    public void getPosition_ValidRequest_ReturnsOkWithPositionDetails() throws Exception {
+        mockMvc.perform(get("/position/2")
+                        .header("Authorization", "f5736401-bcf0-4f04-b896-01777e5d0bb1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(mediaType))
+                .andExpect(jsonPath("$.id").value(2))
+                .andExpect(jsonPath("$.nameOfPosition").value("Existing Test Position name"))
+                .andExpect(jsonPath("$.location").value("Location of Existing Test Position"))
+                .andExpect(jsonPath("$.nameOfClientPostingTheJob").value("existingTestName"));
     }
 
     // endregion
---
+++ src/test/resources/create_positions.sql
@@ -0,0 +1,2 @@
+INSERT INTO positions (id, name, job_location, app_client_api_key) VALUES
+    (2, 'Existing Test Position name', 'Location of Existing Test Position', 'f5736401-bcf0-4f04-b896-01777e5d0bb1');
